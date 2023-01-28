5ee5c95397beadcd43d5ecef0d8e64f3a2751df4
Azmodan <azmodan2@mail.ru>
2023-01-28 15:36:54 +0500

    fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,7 @@
 import './index.scss';
 
 import MainPage from './index.tsx';
-import { ParallaxProvider } from 'react-scroll-parallax';
 
 export default function MyApp() {
-  return (
-    <ParallaxProvider>
-      <MainPage />
-    </ParallaxProvider>
-  );
+  return <MainPage />;
 }
