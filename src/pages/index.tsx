78aae300c86be58493cecfb931a61bb915984e2d
Azmodan <azmodan2@mail.ru>
2023-02-15 21:10:12 +0500

    ховеры, картинки

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -109,11 +109,7 @@ const MainPage = () => {
         cash: '250 000 ₽',
         time: 'плюс лицензия',
       },
-      images: [
-        'UTG-PA-logo_gold 1.svg',
-        'Sfs_1-removebg-preview.png',
-        'Group 25.svg',
-      ],
+      images: ['UTG-PA-logo_gold 1.svg', 'Sverdlovsk.svg', 'Group 25.svg'],
       tags: [
         'Быстрая разработка,',
         'Битрикс,',
@@ -929,7 +925,18 @@ const MainPage = () => {
               <a href="#">
                 <p>Все кейсы</p>
 
-                <img src="/images/arrow2.svg" alt="" />
+                <svg
+                  width="41"
+                  height="8"
+                  viewBox="0 0 41 8"
+                
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M40.3536 4.35355C40.5488 4.15829 40.5488 3.84171 40.3536 3.64645L37.1716 0.464466C36.9763 0.269204 36.6597 0.269204 36.4645 0.464466C36.2692 0.659728 36.2692 0.976311 36.4645 1.17157L39.2929 4L36.4645 6.82843C36.2692 7.02369 36.2692 7.34027 36.4645 7.53553C36.6597 7.7308 36.9763 7.7308 37.1716 7.53553L40.3536 4.35355ZM0 4.5H40V3.5H0V4.5Z"
+                   
+                  />
+                </svg>
               </a>
             </div>
             <div className="carusel_bottom">
