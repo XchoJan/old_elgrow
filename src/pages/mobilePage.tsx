6c5a55f6837dc0f19336962cbb7d9e6f195a560b
sokolov_d <sokolov_d@elgrow.ru>
2023-07-19 20:53:21 +0300

    return scroll-behavior

--- src/pages/mobilePage.tsx
+++ src/pages/mobilePage.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import styles from "../assets/styles/mobilePage.module.css";
-import HeaderMobileComponent from "../Components/HeaderMobileComponent/HeaderMobileComponent";
+import HeaderMobileComponent
+  from "../Components/HeaderMobileComponent/HeaderMobileComponent";
 import MobileMainComponent from "../Components/MobileMainComponent/MobileMainComponent";
-import AboutUsNumbersComponent from "../Components/AboutUsNumbersComponent/AboutUsNumbersComponent";
+import AboutUsNumbersComponent
+  from "../Components/AboutUsNumbersComponent/AboutUsNumbersComponent";
 import AboutUsLetterComponent
   from "../Components/AboutUsLetterComponent/AboutUsLetterComponent";
 import PortfolioComponent from "../Components/PortfolioComponent/PortfolioComponent";
 import HowWeWorkComponent from "../Components/HowWeWorkComponent/HowWeWorkComponent";
 import PriceComponent from "../Components/PriceComponent/PriceComponent";
-import BecomeClientComponent from "../Components/BecomeClientComponent/BecomeClientComponent";
+import BecomeClientComponent
+  from "../Components/BecomeClientComponent/BecomeClientComponent";
 
 const mobilePage = () => {
   return (
       <div className={styles.mobilePage}>
         <HeaderMobileComponent/>
         <div className={styles.container}>
-          <img className={styles.eagleOutstaff} src="/images/sova.png" alt="" />
+          <img className={styles.eagleOutstaff} src="/images/sova.png" alt=""/>
           <MobileMainComponent/>
         </div>
         <AboutUsNumbersComponent/>
