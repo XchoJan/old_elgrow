a945da81feadfa1661dd24344da054c29c5b7479
sokolov_d <sokolov_d@elgrow.ru>
2023-07-20 00:00:08 +0300

    fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 
 import "react-notifications-component/dist/theme.css";
 import {ReactNotifications} from "react-notifications-component";
+import SmoothScroll from "../Components/SmoothScroll";
 
 export default function MyApp({Component, pageProps}) {
   return (
@@ -12,6 +13,7 @@ export default function MyApp({Component, pageProps}) {
           <meta name="viewport" content="width=device-width"/>
           <title>Elgrow</title>
           <link rel="icon" type="image/png" href="/images/favicon.png"/>
+          <SmoothScroll/>
         </Head>
         <Component {...pageProps} />
       </>
