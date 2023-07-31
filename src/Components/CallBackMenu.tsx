5824e3070c012ebd8cfdf937fcf97492da6e1952
sokolov_d <sokolov_d@elgrow.ru>
2023-07-31 13:31:08 +0300

    fixes

--- src/Components/CallBackMenu.tsx
+++ src/Components/CallBackMenu.tsx
@@ -1,10 +1,10 @@
-import { motion } from 'framer-motion';
-import React, { Dispatch, SetStateAction, useState, useEffect } from 'react';
-import Input from './Input';
-import { useRouter } from 'next/router';
-import { useForm, useWatch } from 'react-hook-form';
-import { Store as notification } from 'react-notifications-component';
-import axios from 'axios';
+import {motion} from "framer-motion";
+import React, {Dispatch, SetStateAction, useEffect, useState} from "react";
+import Input from "./Input";
+import {useRouter} from "next/router";
+import {useForm, useWatch} from "react-hook-form";
+import {Store as notification} from "react-notifications-component";
+import axios from "axios";
 
 interface Props {
   isOpen: boolean;
@@ -16,13 +16,13 @@ interface Props {
 }
 
 const CallBackMenu: React.FC<Props> = ({
-  isOpen,
-  close,
-  y,
-  x,
-  childFromMenu,
-  inline,
-}) => {
+                                         isOpen,
+                                         close,
+                                         y,
+                                         x,
+                                         childFromMenu,
+                                         inline,
+                                       }) => {
   const [max, setMax]: any = useState(0);
 
   const router = useRouter();
@@ -32,11 +32,11 @@ const CallBackMenu: React.FC<Props> = ({
     handleSubmit,
     setValue,
     control,
-    formState: { errors },
+    formState: {errors},
   } = useForm<any>({
     defaultValues: {
-      name: '',
-      money: '',
+      name: "",
+      money: "",
     },
   });
 
@@ -45,13 +45,13 @@ const CallBackMenu: React.FC<Props> = ({
     name: `money`,
   });
 
-  if (typeof window !== 'undefined' && isOpen) {
-    let body: any = document.querySelector('body');
-    let callbackMenu: any = document.querySelectorAll('.contacts');
+  if (typeof window !== "undefined" && isOpen) {
+    let body: any = document.querySelector("body");
+    let callbackMenu: any = document.querySelectorAll(".contacts");
 
     setTimeout(() => {
       if (max <= 560) {
-      
+
         callbackMenu[0].scrollTop = 0;
         if (callbackMenu[1]) {
           callbackMenu[1].scrollTop = 0;
@@ -59,47 +59,47 @@ const CallBackMenu: React.FC<Props> = ({
       }
     }, 1000);
 
-    body.style.overflowY = 'hidden';
-  } else if (typeof window !== 'undefined' && !isOpen) {
-    let body: any = document.querySelector('body');
+    body.style.overflowY = "hidden";
+  } else if (typeof window !== "undefined" && !isOpen) {
+    let body: any = document.querySelector("body");
 
-    body.style.overflowY = 'auto';
+    body.style.overflowY = "auto";
   }
 
-  if (typeof window !== 'undefined' && childFromMenu && isOpen) {
+  if (typeof window !== "undefined" && childFromMenu && isOpen) {
   }
 
   useEffect(() => {
     setMax(window?.visualViewport?.width);
   }, []);
 
-  const budgets = ['Менее 0.5 млн', '0.5 - 1 млн', '1 - 3 млн', 'Более 3 млн'];
+  const budgets = ["Менее 0.5 млн", "0.5 - 1 млн", "1 - 3 млн", "Более 3 млн"];
 
   const variants = {
-    open: { y: y, x: x, transition: { duration: 0.8 } },
+    open: {y: y, x: x, transition: {duration: 0.8}},
     closed: {
-      y: '-120%',
+      y: "-120%",
       x: x,
-      transition: { duration: 0.8, delay: 0.2 },
+      transition: {duration: 0.8, delay: 0.2},
     },
   };
 
   const notificationOptionsSuccess: any = {
-    title: 'Заявка отправлена',
-    type: 'success',
-    animationIn: ['animate__animated', 'animate__fadeIn'],
-    animationOut: ['animate__animated', 'animate__fadeOut'],
-    container: 'top-right',
+    title: "Заявка отправлена",
+    type: "success",
+    animationIn: ["animate__animated", "animate__fadeIn"],
+    animationOut: ["animate__animated", "animate__fadeOut"],
+    container: "top-right",
     dismiss: {
       duration: 5000,
     },
   };
   const notificationOptionsError: any = {
-    title: 'Произошла ошибка отправки, попробуйте еще раз',
-    type: 'error',
-    animationIn: ['animate__animated', 'animate__fadeIn'],
-    animationOut: ['animate__animated', 'animate__fadeOut'],
-    container: 'top-right',
+    title: "Произошла ошибка отправки, попробуйте еще раз",
+    type: "error",
+    animationIn: ["animate__animated", "animate__fadeIn"],
+    animationOut: ["animate__animated", "animate__fadeOut"],
+    container: "top-right",
     dismiss: {
       duration: 5000,
     },
@@ -107,7 +107,7 @@ const CallBackMenu: React.FC<Props> = ({
 
   const sendForm = handleSubmit(async (dataForm) => {
     const response = await axios.post(
-      'https://outstaff-server.elgrow.ru/api/elgrow', dataForm,      
+        "https://outstaff-server.elgrow.ru/api/elgrow", dataForm,
     );
     if (response.status !== 200) {
       notification.addNotification({
@@ -117,116 +117,116 @@ const CallBackMenu: React.FC<Props> = ({
       notification.addNotification({
         ...notificationOptionsSuccess,
       });
-      setValue('money', ''),
-        setValue('name', ''),
-        setValue('company', ''),
-        setValue('phone', ''),
-        setValue('comment', '');
+      setValue("money", ""),
+          setValue("name", ""),
+          setValue("company", ""),
+          setValue("phone", ""),
+          setValue("comment", "");
     }
   });
 
   return (
-    <motion.div
-      initial={false}
-      animate={isOpen ? 'open' : 'closed'}
-      variants={variants}
-      className={'callbackMenu'}
-      style={inline}
-    >
-      <motion.div className="callbackMenu_contacts_container">
-        <motion.div className="callbackMenu_contacts">
-          <motion.div className="callbackMenu_contacts_item">
-            <img src="/images/telegram.svg" alt="" />
-            <a href="https://t.me/Elgrow_dev">Написать в Telegram</a>
-          </motion.div>
-          <motion.div className="callbackMenu_contacts_item">
-            <img src="/images/email.svg" alt="" />
-            <a href="mailto:info@elgrow.ru">info@elgrow.ru</a>
-          </motion.div>
-        </motion.div>
-        <motion.div onClick={() => close(false)} className="callbackMenu_close">
-          <img src="/images/close.svg" alt="" />
-        </motion.div>
-      </motion.div>
-      <motion.div className="callbackMenu_main">
-        <form onSubmit={sendForm}>
-          <motion.div className="callbackMenu_main_form">
-            <motion.div
-              animate={isOpen ? 'open' : 'closed'}
-              className="callbackMenu_main_form_fill"
-            >
-              <motion.div animate={isOpen ? 'open' : 'closed'}>
-                <Input
-                  reactHookForm={{ ...register('name', { required: true }) }}
-                  alignItems="center"
-                  textarea={false}
-                  placeholder="Ваше имя"
-                />
-              </motion.div>
-              <motion.div animate={isOpen ? 'open' : 'closed'}>
-                <Input
-                  reactHookForm={{
-                    ...register('company', { required: true }),
-                  }}
-                  alignItems="center"
-                  textarea={false}
-                  placeholder="Компания"
-                />
-              </motion.div>
-              <motion.div animate={isOpen ? 'open' : 'closed'}>
-                <Input
-                  reactHookForm={{ ...register('phone', { required: true }) }}
-                  alignItems="center"
-                  textarea={false}
-                  placeholder="Номер телефона"
-                />
-              </motion.div>
-              <motion.div animate={isOpen ? 'open' : 'closed'}>
-                <Input
-                  reactHookForm={{ ...register('comment', { required: true }) }}
-                  alignItems="center"
-                  textarea={true}
-                  placeholder="Расскажите кратко о задаче"
-                />
-              </motion.div>
-
-              <h4> Бюджет </h4>
-              <motion.div className="callbackMenu_main_form_fill_budget_container">
-                {budgets.map((budget: string, idx: number) => (
-                  <motion.div
-                    onClick={() => setValue('money', budget)}
-                    animate={isOpen ? 'open' : 'closed'}
-                    className={
-                      budgetWatcher === budget
-                        ? 'callbackMenu_main_form_fill_budget_selected'
-                        : 'is callbackMenu_main_form_fill_budget'
-                    }
-                  >
-                    {budget}
-                  </motion.div>
-                ))}
-              </motion.div>
+      <motion.div
+          initial={false}
+          animate={isOpen ? "open" : "closed"}
+          variants={variants}
+          className={"callbackMenu"}
+          style={inline}
+      >
+        <motion.div className="callbackMenu_contacts_container">
+          <motion.div className="callbackMenu_contacts">
+            <motion.div className="callbackMenu_contacts_item">
+              <img src="/images/telegram.svg" alt=""/>
+              <a href="https://t.me/Elgrow_dev">Написать в Telegram</a>
             </motion.div>
-            <motion.button type="submit">Отправить</motion.button>
-
-            {max <= 768 && (
-              <motion.div
-                onClick={() => router.push('/politic')}
-                className="callbackMenu_main_form_politics"
-              >
-                <span> Политика обработки персональных данных </span>
-              </motion.div>
-            )}
+            <motion.div className="callbackMenu_contacts_item">
+              <img src="/images/email.svg" alt=""/>
+              <a href="mailto:info@elgrow.ru">info@elgrow.ru</a>
+            </motion.div>
           </motion.div>
-        </form>
-        <motion.div className="callbackMenu_main_container">
-          <motion.div className="callbackMenu_main_staff">
-            {/* <motion.div className="callbackMenu_main_staff_slogan">
+          <motion.div onClick={() => close(false)} className="callbackMenu_close">
+            <img src="/images/close.svg" alt=""/>
+          </motion.div>
+        </motion.div>
+        <motion.div className="callbackMenu_main">
+          <form onSubmit={sendForm}>
+            <motion.div className="callbackMenu_main_form">
+              <motion.div
+                  animate={isOpen ? "open" : "closed"}
+                  className="callbackMenu_main_form_fill"
+              >
+                <motion.div animate={isOpen ? "open" : "closed"}>
+                  <Input
+                      reactHookForm={{...register("name", {required: true})}}
+                      alignItems="center"
+                      textarea={false}
+                      placeholder="Ваше имя"
+                  />
+                </motion.div>
+                <motion.div animate={isOpen ? "open" : "closed"}>
+                  <Input
+                      reactHookForm={{
+                        ...register("company", {required: true}),
+                      }}
+                      alignItems="center"
+                      textarea={false}
+                      placeholder="Компания"
+                  />
+                </motion.div>
+                <motion.div animate={isOpen ? "open" : "closed"}>
+                  <Input type="number"
+                         reactHookForm={{...register("phone", {required: true})}}
+                         alignItems="center"
+                         textarea={false}
+                         placeholder="Номер телефона"
+                  />
+                </motion.div>
+                <motion.div animate={isOpen ? "open" : "closed"}>
+                  <Input
+                      reactHookForm={{...register("comment", {required: true})}}
+                      alignItems="center"
+                      textarea={true}
+                      placeholder="Расскажите кратко о задаче"
+                  />
+                </motion.div>
+
+                <h4> Бюджет </h4>
+                <motion.div className="callbackMenu_main_form_fill_budget_container">
+                  {budgets.map((budget: string, idx: number) => (
+                      <motion.div
+                          onClick={() => setValue("money", budget)}
+                          animate={isOpen ? "open" : "closed"}
+                          className={
+                            budgetWatcher === budget
+                                ? "callbackMenu_main_form_fill_budget_selected"
+                                : "is callbackMenu_main_form_fill_budget"
+                          }
+                      >
+                        {budget}
+                      </motion.div>
+                  ))}
+                </motion.div>
+              </motion.div>
+              <motion.button type="submit">Отправить</motion.button>
+
+              {max <= 768 && (
+                  <motion.div
+                      onClick={() => router.push("/politic")}
+                      className="callbackMenu_main_form_politics"
+                  >
+                    <span> Политика обработки персональных данных </span>
+                  </motion.div>
+              )}
+            </motion.div>
+          </form>
+          <motion.div className="callbackMenu_main_container">
+            <motion.div className="callbackMenu_main_staff">
+              {/* <motion.div className="callbackMenu_main_staff_slogan">
               <h1>
                 С вами на связи <br /> <span> самые отзвывчивые : </span>
               </h1>
             </motion.div> */}
-            {/* <motion.div className="callbackMenu_main_staff_personnel">
+              {/* <motion.div className="callbackMenu_main_staff_personnel">
               <motion.div className="callbackMenu_main_staff_personnel_person">
                 <img src="/images/miniPerson.png" alt="" />
                 <h2>Михаил Посошнов</h2>
@@ -244,30 +244,30 @@ const CallBackMenu: React.FC<Props> = ({
                 </span>
               </motion.div>
             </motion.div> */}
-            {max > 768 && (
-              <motion.div
-                onClick={() => router.push('/politic')}
-                className="callbackMenu_main_staff_politics"
-              >
-                <span> Политика обработки </span> <br />{' '}
-                <span> персональных данных </span>
-              </motion.div>
-            )}
-          </motion.div>
-          <motion.div className="callbackMenu_main_brief">
-            {/* <motion.div className="callbackMenu_main_brief_text1">
+              {max > 768 && (
+                  <motion.div
+                      onClick={() => router.push("/politic")}
+                      className="callbackMenu_main_staff_politics"
+                  >
+                    <span> Политика обработки </span> <br/>{" "}
+                    <span> персональных данных </span>
+                  </motion.div>
+              )}
+            </motion.div>
+            <motion.div className="callbackMenu_main_brief">
+              {/* <motion.div className="callbackMenu_main_brief_text1">
               или
             </motion.div> */}
-            <motion.div
-              onClick={() => router.push('/brief')}
-              className="callbackMenu_main_brief_text2"
-            >
-              ЗАПОЛНИТЬ БРИФ
+              <motion.div
+                  onClick={() => router.push("/brief")}
+                  className="callbackMenu_main_brief_text2"
+              >
+                ЗАПОЛНИТЬ БРИФ
+              </motion.div>
             </motion.div>
           </motion.div>
         </motion.div>
       </motion.div>
-    </motion.div>
   );
 };
 
---
+++ src/Components/InputPhone.tsx
@@ -0,0 +1,57 @@
+import React, { ReactNode } from 'react';
+import InputMask from "react-input-mask";
+
+interface InputProps {
+  placeholder?: ReactNode ;
+  type?: string;
+  textarea?: boolean;
+  reactHookForm?: any;
+  error?: string | null;
+  labelHeight?: string
+  marginTop?: string;
+  left?: string
+  marginTopLabel?: string
+  alignItems?: string
+}
+
+const Input: React.FC<InputProps> = ({
+  placeholder,
+  type,
+  textarea,
+  error,
+  reactHookForm,
+  labelHeight,
+  marginTop,
+  left,
+  marginTopLabel,
+  alignItems
+}) => {
+  return (
+    <>
+      <label style={{
+        height: labelHeight,
+        marginTop: marginTopLabel,
+        alignItems: alignItems
+      }} className="labelContainer">
+        {textarea ? (
+          <>
+            <textarea style={{marginTop: marginTop,}} required {...reactHookForm} className="textarea" />
+            <span style={{left: left}} className="floatingLabel">{placeholder}</span>
+          </>
+        ) : (
+          <>
+            <input {...reactHookForm} type={type} className="input" required />
+            <InputMask
+                {...reactHookForm}
+                mask='(+7) 999 999 9999'>
+            </InputMask>
+            <span style={{left: left}} className="floatingLabel">{placeholder}</span>
+          </>
+        )}
+      </label>
+      {error && <h5 className="error">{error}</h5>}
+    </>
+  );
+};
+
+export default Input;
